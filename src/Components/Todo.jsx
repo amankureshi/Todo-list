2e0caabd68dd0651489fd96c44ea29fb15a41f7d
Aman Kureshi <amankureshi78692@gmail.com>
2024-09-12 16:39:43 +0530

    input field complated

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,7 +1,34 @@
-import React from "react";
-
+import React, { useState } from "react";
 const Todo = () => {
-  return <h1>Todo list</h1>;
+  const [initial, setInitial] = useState();
+  const [data, setData] = useState([]);
+  const getInput = (e) => {
+    setInitial(e.target.value);
+  };
+  const getData = () => {
+    console.log(initial);
+    let store = [...data, initial];
+    setData(store);
+    setInitial("");
+  };
+  return (
+    <div>
+      <div className="container">
+        <div className="inputTask">
+          <input
+            type="text"
+            placeholder="Enter your task"
+            onChange={getInput}
+            value={initial}
+          />
+          <button className="add-btn" onClick={getData}>
+            Add
+          </button>
+        </div>
+        <h1>{data}</h1>
+      </div>
+    </div>
+  );
 };
 
 export default Todo;
