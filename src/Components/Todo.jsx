280c511af52d0fc6e2573ac5f6fe7da0878b37d6
Aman Kureshi <amankureshi78692@gmail.com>
2024-09-13 17:48:14 +0530

    image add with animation

--- src/Components/Todo.jsx
+++ src/Components/Todo.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from "react";
+import { MdDelete } from "react-icons/md";
+
 const Todo = () => {
-  const [initial, setInitial] = useState();
+  const [initial, setInitial] = useState("");
   const [data, setData] = useState([]);
+  const [isHovered, setIsHovered] = useState(false);
   const getInput = (e) => {
     setInitial(e.target.value);
   };
@@ -10,9 +13,36 @@ const Todo = () => {
     let store = [...data, initial];
     setData(store);
     setInitial("");
+    console.log(store);
   };
+  const deleteTask = (indexValue) => {
+    console.log(indexValue);
+    let filterData = data.filter((curElement, id) => {
+      return id !== indexValue;
+    });
+    setData(filterData);
+  };
+  const startAnimation = () => {
+    setIsHovered(true);
+  };
+
+  const stopAnimation = () => {
+    setTimeout(() => {
+      setIsHovered(false);
+    }, 3000);
+  };
+
   return (
     <div>
+      <div className="todoList">
+        <h1>ToDo List</h1>
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/7692/7692809.png"
+          className={`todoImage ${isHovered ? "animate" : ""}`}
+          onMouseOver={startAnimation}
+          onMouseLeave={stopAnimation}
+        />
+      </div>
       <div className="container">
         <div className="inputTask">
           <input
@@ -25,7 +55,17 @@ const Todo = () => {
             Add
           </button>
         </div>
-        <h1>{data}</h1>
+        {data.map((curValue, index) => {
+          console.log(index);
+          return (
+            <>
+              <div className="taskData animate-task" key={index}>
+                <p>{curValue}</p>
+                <MdDelete id="deleteIcon" onClick={() => deleteTask(index)} />
+              </div>
+            </>
+          );
+        })}
       </div>
     </div>
   );
